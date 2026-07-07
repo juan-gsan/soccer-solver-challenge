f099b7aa218c1b9e06cb04df0527a8da6e6500bc
JuanG <juan.gsanchez13@gmail.com>
2026-07-07 10:32:37 +0200

    Fix button go home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import PlayerSearchPage from "./pages/PlayerSearchPage";
 import PlayerEvolutionPage from "./pages/PlayerEvolutionPage";
 import ComparePage from "./pages/ComparePage";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="app">
       <nav className="navbar">
         <Link to="/">SoccerSolver - Player Search</Link>
-        <Link to="/compare">Evolution Comparison</Link>
+        <Link to="/compare">Player Comparison</Link>
       </nav>
       <main>
         <Routes>
-          <Route path="/" element={<PlayerSearchPage />} />
+          <Route path="/" element={<PlayerSearchPage key={location.key} />} />
           <Route
             path="/players/:playerId/evolution"
             element={<PlayerEvolutionPage />}
